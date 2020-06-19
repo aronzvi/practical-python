3da32ab0d69a68debf2722a3ca0170194ba5c7d7
Aron Weiner <aronzvi@gmail.com>
2020-06-20 01:36:55 +0700

    Exercise 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,6 +1,6 @@
 # mortgage.py
 #
-# Exercise 1.10
+# Exercise 1.11
 
 principal = 500000.0
 rate = 0.05
@@ -22,13 +22,15 @@ while principal > 0:
     month_count += 1 
     if (month_count >= extra_payment_start_month) and (month_count <= extra_payment_end_month):
         payment = regular_payment + extra_payment
-        #print('Extra payment month', month_count)
     else:
         payment = regular_payment
 
-    principal = principal * (1+rate/12) - payment 
+    principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
     print(month_count, total_paid, principal)
-       
+
+if principal < 0:
+        print('Correcting last month overpayment by', abs(principal))
+        total_paid = total_paid - abs(principal)
 print('Total paid', total_paid)
 print('Months', month_count)
