adf55708aca8c8e69cac1cb222c8896dcb1367a5
Aron Weiner <aronzvi@gmail.com>
2020-06-20 01:16:51 +0700

    Exercise 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,24 +1,32 @@
 # mortgage.py
 #
-# Exercise 1.8
+# Exercise 1.9
 
 principal = 500000.0
 rate = 0.05
 regular_payment = 2684.11
 total_paid = 0.0
-number_of_months = 0
+month_count = 0
 extra_payment = 1000
-number_of_extra_payments = 12
+
+""" 
+How much will Dave pay if he pays an extra $1000/month for 4 years starting in year 5 of the mortgage?
+"""
+extra_payment_start_month = 49
+extra_payment_end_month = 96
+
+print('Start month of extra', extra_payment_start_month)
+print('End month of extra', extra_payment_end_month)
 
 while principal > 0:
-    if number_of_extra_payments > 0:
+    month_count += 1 
+    if (month_count >= extra_payment_start_month) and (month_count <= extra_payment_end_month):
         payment = regular_payment + extra_payment
-        number_of_extra_payments -= 1
+        print('Extra payment month', month_count)
     else:
         payment = regular_payment
 
     principal = principal * (1+rate/12) - payment 
     total_paid = total_paid + payment
-    number_of_months += 1    
-
-print('Total paid', total_paid, 'Number of months required', number_of_months)
+       
+print('Total paid', total_paid, 'Number of months required', month_count)
