191f1149860f52b60692613f9b4a55452bbd2974
Aron Weiner <aronzvi@gmail.com>
2020-06-21 23:10:17 +0700

    Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,21 @@
 # pcost.py
 #
-# Exercise 1.27
-total_cost = 0
+# Exercise 1.30
 
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f)
-for line in f:
-    row = line.split(',')
-    shares = int(row[1])
-    price = float(row[2].strip())
-    cost = shares * price
-    total_cost += cost
+def portfolio_cost(filename):
+    total_cost = 0
 
-f.close()
-print(f'Total cost {total_cost}')
+    f = open(filename, 'rt')
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        shares = int(row[1])
+        price = float(row[2].strip())
+        cost = shares * price
+        total_cost += cost
+
+    f.close()
+    return total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost {cost}')
