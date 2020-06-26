c23a4e7e7f3e45f2c5e30209ceab8a0161995477
Aron Weiner <aronzvi@gmail.com>
2020-06-26 17:50:18 +0700

    Exercise 2.6

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.5
+# Exercise 2.6
 
 import csv
 
@@ -14,3 +14,15 @@ def read_portfolio(filename):
                 portfolio.append(holding)
 
     return portfolio
+
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                print('Error: bad value for row')
+                pass
+    return prices
