47e51a43367cff6a53e8422a3074845d42e65685
Aron Weiner <aronzvi@gmail.com>
2020-07-21 02:21:42 +0700

    Exercise 3.4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,19 +1,30 @@
 # fileparse.py
 #
-# Exercise 3.3
+# Exercise 3.4
 
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
     
     with open(filename) as f:
         rows = csv.reader(f)
 
         headers = next(rows)
+
+        if select:
+            indices = [headers.index(col_name) for col_name in select]
+            headers = select
+        else:
+            indices = [] 
+
         records = []
         for row in rows:
             if not row:
-                continue    
+                continue
+
+            if indices:
+                row = [row[index] for index in indices]
+
             record = dict(zip(headers, row))
             records.append(record)
 
