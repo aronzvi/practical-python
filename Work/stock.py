9d8de9dd9df6013bae5c06d88a299f48d2bd0e8e
Aron Weiner <aronzvi@gmail.com>
2020-07-30 17:45:35 +0700

    Exercise 5.8

--- Work/stock.py
+++ Work/stock.py
@@ -1,6 +1,7 @@
-# Exercise 4.9
+# Exercise 5.8
 
 class Stock:
+    __slots__ = ('name', '_shares', 'price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -9,8 +10,19 @@ class Stock:
     def sell(self, nshares):
         self.shares -= nshares
 
+    @property
     def cost(self):
         return self.shares * self.price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, val):
+        if not isinstance(val, int):
+            raise TypeError('Expected int')
+        self._shares = val
 
     def __repr__(self):
         return f"Stock('{self.name}', {self.shares}, {self.price})"
