cdcee18a8a9e5270b9a0afcbd73442b9471762ad
Aron Weiner <aronzvi@gmail.com>
2020-08-03 00:55:57 +0700

    Exercise 6.15

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,4 +1,4 @@
-# Exercise 6.12
+# Exercise 6.15
 
 from follow import follow
 import csv
@@ -14,13 +14,7 @@ def convert_types(rows, types):
         yield [func(val) for func, val in zip(types, row)]
 
 def make_dicts(rows, headers):
-    for row in rows:
-        yield dict(zip(headers, row))
-
-def filter_symbols(rows, names):
-    for row in rows:
-        if row['name'] in names:
-            yield row
+    return (dict(zip(headers, row)) for row in rows)
 
 def parse_stock_data(lines):
     rows = csv.reader(lines)
@@ -33,8 +27,7 @@ def ticker(portfile, logfile, fmt):
     portfolio = report.read_portfolio(portfile)
     lines = follow(logfile)
     rows = parse_stock_data(lines)
-    rows = filter_symbols(rows, portfolio)
-    
+    rows = (row for row in rows if row['name'] in portfolio)
     formatter = tableformat.create_formatter(fmt)
     formatter.headings(['Name' ,'Price' ,'Change'])
     for row in rows:
